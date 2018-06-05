8d0622a8be5eaa05af099c6c07b440a5a6aee91a
Mike Boan <mikeboan@gmail.com>
2018-06-05 16:51:09 -0400

    render list of heroes and individual hero, keep track of previously selected hero

--- src/app/heroes/hero-detail.component.ts
+++ src/app/heroes/hero-detail.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Hero, HeroService } from './hero.service';
+import { switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 
 @Component({
-  template: `<p>todo: hero detail component</p>`,
+  template: `
+    <h1>Heroes</h1>
+    <ng-container *ngIf="hero$ | async as hero">
+      <h2>{{hero.name}}</h2>
+      <p>id: {{hero.id}}</p>
+      <p>name: <input [(ngModel)]="hero.name"></p>
+
+      <button (click)="goBack(hero)">Back</button>
+    </ng-container>
+  `,
 })
 export class HeroDetailComponent implements OnInit {
+  hero$: Observable<Hero>;
 
   constructor(
-    // todo: inject router and hero service
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private heroService: HeroService
   ) {}
 
   ngOnInit() {
-    // todo: fetch individual hero
+    // snapshot is fine here because we don't reuse the component instance
+    //    the user must navigate back to /heroes in order to get to another hero detail component (which will be a fresh instance)
+    const heroId = this.activatedRoute.snapshot.paramMap.get('id');
+
+    this.hero$ = this.heroService.getHero(heroId);
+  }
+
+  goBack(hero: Hero) {
+    const heroId = hero ? hero.id : null; // argument from template may be null
+
+    this.router.navigate(['/heroes', { prevHeroId: heroId }]);
   }
 }
--- src/app/heroes/hero-list.component.ts
+++ src/app/heroes/hero-list.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Hero, HeroService } from './hero.service';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   template: `
     <h2>HEROES</h2>
-  `
+    <ul class="items">
+      <li *ngFor="let hero of heroes$ | async"
+        [class.selected]="hero.id === selectedHeroId">
+        <a [routerLink]="['/heroes', hero.id]">
+          <span class="badge">{{ hero.id }}</span>{{ hero.name }}
+        </a>
+      </li>
+    </ul>
+
+    <button routerLink="/sidekicks">Go to sidekicks</button>
+  `,
+  styles: [`.selected { background-color: pink }`]
 })
-export class HeroListComponent {}
+export class HeroListComponent implements OnInit {
+  heroes$: Observable<Hero[]>;
+
+  private selectedHeroId: number;
+
+  constructor(
+    private heroService: HeroService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.heroes$ = this.activatedRoute.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        this.selectedHeroId = +params.get('prevHeroId');
+        return this.heroService.getHeroes(); // must return observable from cb passed to switchMap
+      })
+    );
+  }
+}
