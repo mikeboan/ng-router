311bac85f644ff6aff15b4744614b975c83af962
Mike Boan <mikeboan@gmail.com>
2018-06-05 15:36:28 -0400

    add wildcard not found route and component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import {FormsModule} from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const appRoutes: Routes = [
   { path: 'crisis-center', component: CrisisListComponent },
-  { path: 'heroes', component: HeroListComponent }
+  { path: 'heroes', component: HeroListComponent },
+  { path: '**', component: NotFoundComponent },
   ];
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroListComponent
+    HeroListComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
